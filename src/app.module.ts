a8fb21eee8781581cea08f5bbdbc445aacb11097
FatihBARACKILIC <fatihb469@gmail.com>
2024-01-31 15:21:33 +0000

    cleaned

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { CatsModule } from './cats/cats.module';
-import { DogsModule } from './dogs/dogs.module';
 
 @Module({
-  imports: [CatsModule, DogsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
